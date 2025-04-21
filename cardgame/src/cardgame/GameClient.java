d8ef6409a1b897027f8c9250677f9528e007d877
purpleglassbottle <reignzblock@gmail.com>
2025-04-21 01:13:16 -0400

    Refactor game architecture and integrate network communication for multiplayer support

--- cardgame/src/cardgame/GameClient.java
+++ cardgame/src/cardgame/GameClient.java
@@ -1,9 +1,33 @@
 package cardgame;
 
-/**
- *
- * @author reign
- */
+import java.io.PrintWriter;
+import java.io.IOException; 
+import java.net.Socket;
+
 public class GameClient {
-    
+    private Socket socket;
+    private PrintWriter out;
+
+    public GameClient(String host, int port) throws IOException {
+        this.socket = new Socket(host, port);
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public void sendPlayCard(int index, String color) {
+        String message = "PLAY_CARD " + index + " " + color;
+        out.println(message);
+//        System.out.println("[Sent to Server] " + message);
+    }
+
+    public void sendEndGame() {
+        out.println("END_GAME");
+    }
+
+    public void closeConnection() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
